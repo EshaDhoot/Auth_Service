0dc91c19467c2d0fab4d3c28cf6c95af2fec35e7
Esha <maheshwariesha11@gmail.com>
2024-01-10 22:33:03 +0530

    First commit: project skeleton setup done

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const { PORT } = require('./config/serverConfig');
+
+const app = express();
+
+const prepareAndStartServer = async () => {
+    app.listen(PORT, () => {
+        console.log(`Server Started on Port: ${PORT}`);
+    })
+}
+
+prepareAndStartServer();
