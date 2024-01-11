97008f848781d4f5c1d17743c7b84b52a7904d56
Esha <maheshwariesha11@gmail.com>
2024-01-11 20:47:58 +0530

    Added jwt create and verify token

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
-// const UserRepository = require('./repository/user-repository');
+
 const app = express();
 
 const prepareAndStartServer = async () => {
@@ -13,13 +13,14 @@ const prepareAndStartServer = async () => {
 
     app.use('/api',ApiRoutes);
 
-    // const repo = new UserRepository();
-    // const response = await repo.getById(1);
-    // console.log(response);
+   
 
     app.listen(PORT, () => {
         console.log(`Server Started on Port: ${PORT}`);
-    })
+       
+    });
+
+
 }
 
 prepareAndStartServer();
