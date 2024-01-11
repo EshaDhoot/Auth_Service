5a791e524bebfa568c4f3b18429f5af37d4996f9
Esha <maheshwariesha11@gmail.com>
2024-01-11 22:04:22 +0530

    Added token generation logic

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -21,10 +21,31 @@ const create = async (req,res) => {
             data: {},
             success: false,
             err: error
-        })
+        });
+    }
+}
+
+const signIn = async (req, res) => {
+    try {
+        const response = await userService.signIn(req.body.email, req.body.password);
+        return res.status(200).json({
+            message: 'Successsully signed in',
+            success: true,
+            data: response,
+            err: {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Something went wrong',
+            data: {},
+            success: false,
+            err: error
+        });
     }
 }
 
 module.exports = {
-    create
+    create,
+    signIn
 }
